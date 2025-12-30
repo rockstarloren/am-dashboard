773d4d012148a7602e90b5e3b6b3c8c57a61c810
lorenperalta.rwds <lorenperalta.rwds@gmail.com>
2025-12-30 13:37:23 +0000

    Updated the design

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,7 +7,7 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
+    pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
@@ -25,16 +25,10 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          command = ["python3" "-m" "http.server" "$PORT"];
+          manager = "web";
+        };
       };
     };
 
